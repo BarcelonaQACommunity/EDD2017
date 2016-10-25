8cbd1b578e75acc64216b08266688345cd3d7419
nyoro <nyoro@DESKTOP-DJ6JE2M>
2016-10-25 20:09:13 +0200

    Add project files.

---
+++ PageObject.Factory/Base/PageObjectBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace PageObject.Factory.Base
+{
+    /// <summary>
+    /// The Page Object base.
+    /// </summary>
+    public class PageObjectBase
+    {
+        protected IWebDriver WebDriver;
+
+        public PageObjectBase(IWebDriver webDriver)
+        {
+            this.WebDriver = webDriver;
+        }
+    }
+}
