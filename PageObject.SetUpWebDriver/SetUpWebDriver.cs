7fa9f2ac5b8da4d5c383191492fac07ad311e20c
NyoronDev <jsernajaen@gmail.com>
2017-01-10 23:55:37 +0100

    Add saucelabs cloud testing

--- PageObject.SetUpWebDriver/SetUpWebDriver.cs
+++ PageObject.SetUpWebDriver/SetUpWebDriver.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 
 namespace PageObject.SetUpWebDriver
 {
@@ -29,14 +30,29 @@ namespace PageObject.SetUpWebDriver
         }
 
         /// <summary>
+        /// Sets up sauce labs web driver.
+        /// </summary>
+        public static void SetUpSauceLabsWebDriver()
+        {
+            var caps = new DesiredCapabilities();
+            caps.SetCapability(CapabilityType.BrowserName, "chrome");
+            caps.SetCapability(CapabilityType.Version, "45");
+            caps.SetCapability(CapabilityType.Platform, "Windows 10");
+            caps.SetCapability("deviceName", "");
+            caps.SetCapability("deviceOrientation", "");
+            caps.SetCapability("username", "nyoronsheppard");
+            caps.SetCapability("accessKey", "ba81bd05-eb98-4a41-a837-65817ccc8bc1");
+
+            WebDriver = new RemoteWebDriver(new Uri("http://ondemand.saucelabs.com:80/wd/hub"), caps, TimeSpan.FromSeconds(600));
+        }
+
+
+        /// <summary>
         /// Closes the chrome web driver.
         /// </summary>
         public static void CloseChromeWebDriver()
         {
-            if (WebDriver != null)
-            {
-                WebDriver.Dispose();
-            }
+            WebDriver?.Dispose();
         }
     }
 }
